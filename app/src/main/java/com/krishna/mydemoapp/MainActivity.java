2831592bbe9829502e33441dddcf31f96ba3d3d0
Parth <parth1493@gmail.com>
2017-08-23 23:01:07 -0400

    Moshi Example

--- app/src/main/java/com/krishna/mydemoapp/MainActivity.java
+++ app/src/main/java/com/krishna/mydemoapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.krishna.mydemoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.krishna.mydemoapp.example.moshi.MoshiExample;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView moshiExmaple;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        xmlBinder();
+        moshiExmaple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MoshiExample.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void xmlBinder() {
+        moshiExmaple = (TextView)findViewById(R.id.moshi_exmaple);
     }
 }
