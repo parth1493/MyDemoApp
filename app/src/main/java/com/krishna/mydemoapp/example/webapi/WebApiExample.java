c0ce118b846459892b4da6671f454ba0f6335645
Parth <parth1493@gmail.com>
2017-08-24 12:01:51 -0400

    AsyncTask Added

--- app/src/main/java/com/krishna/mydemoapp/example/webapi/WebApiExample.java
+++ app/src/main/java/com/krishna/mydemoapp/example/webapi/WebApiExample.java
@@ -1,15 +1,24 @@
 package com.krishna.mydemoapp.example.webapi;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import com.krishna.mydemoapp.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.HashMap;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -17,40 +26,60 @@ import okhttp3.Response;
 
 public class WebApiExample extends AppCompatActivity {
 
+    private String TAG = WebApiExample.class.getSimpleName();
     OkHttpClient client = new OkHttpClient();
-
+    private ProgressDialog pDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_api_example);
 
-        new AsyncTask<Void, Void, String>() {
-            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-            @Override
-            protected String doInBackground(Void... params) {
-//                OkHttpClient client = new OkHttpClient();
-//                Request request = new Request.Builder()
-//                        .url("https://api.github.com/users/ashokslsk")
-//                        .build();
-
-//                try (Response response = client.newCall(request).execute()) {
-//                    Log.d("Json", "doInBackground() called with: " + "params = [" + response.body().string() + "]");
-//                   // return response.body().string();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-
-                GetDataExample example = new GetDataExample();
-                try {
-                    String response = example.run("https://api.androidhive.info/contacts/");
-                    Log.d("Json", "doInBackground() called with: " + "params = [" + response + "]");
-//
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-        }.execute();
+        new GetContacts().execute();
     }
 
+
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(WebApiExample.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+
+            GetDataExample example = new GetDataExample();
+            try {
+                String response = example.run("https://api.androidhive.info/contacts/");
+                Log.d("Json", "doInBackground() called with: " + "params = [" + response + "]");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+
+
+        }
+
+    }
 }
